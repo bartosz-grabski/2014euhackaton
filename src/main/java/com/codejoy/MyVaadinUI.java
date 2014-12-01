3b5699ead43b8610ed2859e7026ad50556c2d477
Bartosz Grabski <b.grabski92@gmail.com>
2014-12-02 00:30:34 +0100

    initial setup

---
+++ src/main/java/com/codejoy/MyVaadinUI.java
@@ -0,0 +1,48 @@
+package com.codejoy;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("mytheme")
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI
+{
+
+    @WebServlet(value = "/*", asyncSupported = true)
+    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.codejoy.AppWidgetSet")
+    public static class Servlet extends VaadinServlet {
+    }
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        
+        Chart chart = new Chart(ChartType.COLUMN);
+        chart.setWidth("300px");
+        chart.setHeight("400px");
+        
+        layout.setMargin(true);
+        layout.addComponent(chart);
+        setContent(layout);
+        
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
+    }
+
+}
